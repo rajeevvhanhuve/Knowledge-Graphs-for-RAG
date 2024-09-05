54e6670080416d751e0dac254ae00756c828f5bd
“rajeev <rajeevvhanhuve@gmail.com>
2024-09-06 00:36:46 +0530

    Code updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Knowledge Graph Project',
+    version= '0.0.0',
+    author= 'Rajeev Vhanhuve',
+    author_email= 'rajeevvhanhuve@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
